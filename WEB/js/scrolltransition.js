33d064eae538d7f8bd1164e8df82cc16e91a9e78
吳孟庭 <b98602046@ntu.edu.tw>
2016-10-15 12:27:14 +0800

    initial project

---
+++ WEB/js/scrolltransition.js
@@ -0,0 +1,14 @@
+
+    var wrap = $("#wrap");
+    var logo = $(".logo");
+
+$(window).on("scroll", function(e) {
+    // console.log(this.scrollY)
+  if (this.scrollY > 45) {
+    logo.addClass("logo2");
+  } else {
+    logo.removeClass("logo2");
+  }
+  
+});
+
