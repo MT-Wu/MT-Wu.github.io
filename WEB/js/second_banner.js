3d85310d661dd302a0c2c9a0c878fc0d9a162524
吳孟庭 <b98602046@ntu.edu.tw>
2016-10-29 01:05:45 +0800

    修正第二選單動畫的區段位置，改為隨RWD取變數更為精準

--- WEB/js/second_banner.js
+++ WEB/js/second_banner.js
@@ -12,27 +12,33 @@ $('.secondbanner li').click(function(){
 // team分頁的第二選單動畫
 var secondbanner_ul = $(".secondbanner ul");
 
-$window.on("scroll", function(e) {
-	
+
+
+$window.on("scroll resize", function(e) {
+
 	// 依照實際排版偵測各個區段的界限
     // console.log(this.scrollY);
+
+    var RAtop = $('h2').eq(1).offset().top - 265 ; //動態抓RA標籤的位置
+	var PHDtop = $('h2').eq(2).offset().top - 265 ; //動態抓PHD標籤的位置
+	var MStop = $('h2').eq(3).offset().top - 265 ; //動態抓MS標籤的位置
 	
 	// 移到第二個標籤
-    if (this.scrollY >= 485 && this.scrollY < 1660){
+    if (this.scrollY >= RAtop && this.scrollY < PHDtop){
       secondbanner_ul.addClass('RA');
     }else{
       secondbanner_ul.removeClass('RA');
     }
 
 	// 移到第三個標籤
-    if (this.scrollY >= 1660 && this.scrollY < 2915){
+    if (this.scrollY >= PHDtop && this.scrollY < MStop){
       secondbanner_ul.addClass('PHD');
     }else{
       secondbanner_ul.removeClass('PHD');
     }
 
 	// 移到第四個標籤
-    if (this.scrollY >= 2915){
+    if (this.scrollY >= MStop){
       secondbanner_ul.addClass('MS');
     }else{
       secondbanner_ul.removeClass('MS');
